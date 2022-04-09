a68b980f0607630c8224c88e7719a106228e4c09
Lue Xiong <luexiong@pop-os.localdomain>
2022-04-09 11:16:30 -0500

    Add db container fixture for tests

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,13 +11,12 @@ if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ];
 then
   echo "Container $CONTAINER_NAME is already running"
 else
-  docker build -t $TAG_NAME .
   docker run --name $CONTAINER_NAME -d -p 5432:5432 $TAG_NAME
   sleep 1.5
 fi
 
 export PGSERVICEFILE=.pg_service.conf
 export PGPASSFILE=.pgpass
-./manage.py makemigrations polls
+yes | ./manage.py makemigrations polls
 ./manage.py migrate
 ./manage.py runserver
--- polls/tests/conftest.py
+++ polls/tests/conftest.py
@@ -1,6 +1,34 @@
+from time import sleep
+import docker
 import pytest
 from faker import Faker
 from rest_framework.test import APIClient
+
+import path
+
+
+CONTAINER_NAME = "electiify-db-container"
+TAG_NAME = "electiify-db"
+PORT = "5432"
+
+
+# TODO: Find better solution instead of hard-timed sleep to
+#  give time for container to be in running state
+@pytest.fixture(scope="session", autouse=True)
+def db_container():
+    client = docker.from_env()
+
+    containers = client.containers.list(filters={"name": CONTAINER_NAME})
+    if not containers:
+        image = client.images.build(path=path.PROJECT_DIRECTORY, tag=TAG_NAME)
+        client.containers.run(
+            image[0],
+            name=CONTAINER_NAME,
+            detach=True,
+            ports={f"{PORT}/tcp": PORT},
+        )
+
+        sleep(1.5)
 
 
 @pytest.fixture
